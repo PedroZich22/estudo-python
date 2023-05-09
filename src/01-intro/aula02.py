3c2a8ba52d04aa4badf8c16f587b6a8e9db5202a
Pedro Zich <zichpedro22@gmail.com>
2023-05-09 18:21:23 -0300

    aula 02 - keywords e identificadores

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,37 @@
+""" Aula 2 - Keywords e identificadores"""
+
+# Palavras reservadas
+# True / False / None / and / import / lambda
+
+# identificadores
+# nome de variaveis / funcoes / classes
+# case sensitive
+# devem iniciar com letra ou _
+# só podem conter letras, numeros e _
+# nao pode conter espacos e, branco
+# sem caracteres especiais
+
+nome = 'Pedro'
+idade = 17
+Nome = 'Joao'
+nome_completo = 'Pedro Zich'
+
+
+# boas praticas / clean code
+
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# constantes
+PI = 3.14
+MAIORIDADE = 18
+
+idade = 18
+
+if (idade >= MAIORIDADE):
+    print('Voce e maior de idade')
+else:
+    print('Voce e menor de idade')
