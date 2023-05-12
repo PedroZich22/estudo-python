c35147c92d3260cfddfde6cb4e4c9649eda41030
Pedro Zich <zichpedro22@gmail.com>
2023-05-11 22:51:42 -0300

    correcao ex03-controle-de-fluxo

--- src/02-controle-de-fluxo/exercicios/ex03.py
+++ src/02-controle-de-fluxo/exercicios/ex03.py
@@ -1,2 +1,19 @@
+def verificar_codigo(codigo):
+    """Verifica se o codigo esta correto"""
+    if len(codigo) != 7:
+        return "Codigo invalido!"
+    elif codigo[:2] != "BR":
+        return "Codigo invalido!"
+    elif codigo[-1] != "X":
+        return "Codigo invalido!"
+    else:
+        try:
+            codigo = int(codigo[3:6])
+            return "O codigo é valido!"
+        except:
+            return "Codigo invalido!"
+        
+
 codigo = input('Digite o código para avaliação: ')
 
+print(verificar_codigo(codigo))
