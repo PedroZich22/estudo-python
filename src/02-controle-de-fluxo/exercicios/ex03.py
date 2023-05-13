cbfe039d3be1cb2867278119f0e28e245af9d9bc
Pedro Zich <zichpedro22@gmail.com>
2023-05-13 11:08:22 -0300

    melhoria exercicio 03-controle-de-fluxo

--- src/02-controle-de-fluxo/exercicios/ex03.py
+++ src/02-controle-de-fluxo/exercicios/ex03.py
@@ -1,19 +1,17 @@
-def verificar_codigo(codigo):
-    """Verifica se o codigo esta correto"""
-    if len(codigo) != 7:
-        return "Codigo invalido!"
-    elif codigo[:2] != "BR":
-        return "Codigo invalido!"
-    elif codigo[-1] != "X":
-        return "Codigo invalido!"
-    else:
-        try:
-            codigo = int(codigo[3:6])
-            return "O codigo é valido!"
-        except:
-            return "Codigo invalido!"
-        
+
+"""Verifica se o codigo esta correto"""
 
 codigo = input('Digite o código para avaliação: ')
 
-print(verificar_codigo(codigo))
+if len(codigo) != 7:
+    print( "Codigo invalido!")
+elif codigo[:2] != "BR":
+    print( "Codigo invalido!")
+elif codigo[-1] != "X":
+    print( "Codigo invalido!")
+else:
+    try:
+        codigo = int(codigo[3:6])
+        print("O codigo é valido!")
+    except TypeError as erro:
+        print("Codigo invalido!")
