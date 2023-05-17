5905d2ef1bf3a8bae2061f947c8a83c77d601123
Pedro Zich <zichpedro22@gmail.com>
2023-05-16 21:19:48 -0300

    aula01 - manipulando arquivos

--- src/06-arquivos/aula01.py
+++ src/06-arquivos/aula01.py
@@ -7,8 +7,32 @@
 # x - criar arquivo
 # r+ - write/read
 
-arquivo = open("src/06-arquivos/test.txt", "r", encoding='utf-8')
+# arquivo = open("src/06-arquivos/test3.txt", "x", encoding='utf-8')
 
-print(arquivo.readable())
+# READ
 
-arquivo.close()
+# print(arquivo.readable())
+# print(arquivo.read())
+# print(arquivo.readline())
+# print(arquivo.readline())
+# print(arquivo.readline())
+# print(arquivo.readline())
+
+# lista = arquivo.readlines()
+# print(lista)
+# print(lista[2])
+
+# APPEND
+
+# arquivo.write("Python\n")
+
+# arquivo.close()
+
+import os 
+
+# if os.path.exists("src/06-arquivos/test2.txt"):
+#     os.remove("src/06-arquivos/test2.txt")
+# else:
+#     print("Arquivo não existe")
+
+os.rmdir("src/06-arquivos/nova_pasta")
